8b729ce44a89dd165f533509afd6a0a62b7c3c19
Tor <157131052+tlhaugen@users.noreply.github.com>
2025-11-09 18:08:59 +0100

    Somewhat working task 2

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def forward_diff_x_boundary(I):
     n, m = I.shape
     dx = np.zeros_like(I)
@@ -34,6 +35,7 @@ def compute_derivatives_blur(I0, I1, sigma):
 def build_rhs(Ix, Iy, It):
     return -It * Ix, -It * Iy
 
+
 def laplacian5_dirichlet(u):
     n, m = u.shape
     L = np.zeros_like(u)
@@ -50,34 +52,61 @@ def apply_A(u, v, Ix, Iy, reg):
     Lv = laplacian5_dirichlet(v)
     Au = (Ix * Ix) * u + (Ix * Iy) * v - reg * Lu
     Av = (Ix * Iy) * u + (Iy * Iy) * v - reg * Lv
+ 
     Au[0, :] = 0.0; Au[-1, :] = 0.0; Au[:, 0] = 0.0; Au[:, -1] = 0.0
     Av[0, :] = 0.0; Av[-1, :] = 0.0; Av[:, 0] = 0.0; Av[:, -1] = 0.0
     return Au, Av
 
+def enforce_bc(u, v):
+    u[0, :] = 0.0; u[-1, :] = 0.0; u[:, 0] = 0.0; u[:, -1] = 0.0
+    v[0, :] = 0.0; v[-1, :] = 0.0; v[:, 0] = 0.0; v[:, -1] = 0.0
+
+
 def of_cg(u0, v0, Ix, Iy, reg, rhsu, rhsv, tol=1e-8, maxit=2000, callback=None):
     u = u0.copy(); v = v0.copy()
+
+    # initial residual
     Au, Av = apply_A(u, v, Ix, Iy, reg)
-    ru = rhsu - Au; rv = rhsv - Av
+    ru = rhsu - Au
+    rv = rhsv - Av
     r2_0 = float(np.sum(ru * ru + rv * rv))
     if r2_0 == 0.0:
+        enforce_bc(u, v)
         return u, v, 0, 0.0
+
     pu = ru.copy(); pv = rv.copy()
+    enforce_bc(pu, pv)
+
     r2_old = r2_0
     for it in range(1, maxit + 1):
         Apu, Apv = apply_A(pu, pv, Ix, Iy, reg)
         denom = float(np.sum(pu * Apu + pv * Apv))
         if denom <= 0.0:
             break
+
         alpha = r2_old / denom
-        u += alpha * pu; v += alpha * pv
-        ru -= alpha * Apu; rv -= alpha * Apv
+
+        # update and enforce Dirichlet on the solution
+        u += alpha * pu
+        v += alpha * pv
+        enforce_bc(u, v)
+
+        # residual
+        ru -= alpha * Apu
+        rv -= alpha * Apv
+
         r2_new = float(np.sum(ru * ru + rv * rv))
         rel = r2_new / r2_0
         if callback is not None:
             callback(it, rel)
         if rel < tol:
             return u, v, it, rel
+
         beta = r2_new / r2_old
-        pu = ru + beta * pu; pv = rv + beta * pv
+        pu = ru + beta * pu
+        pv = rv + beta * pv
+        enforce_bc(pu, pv)  # keep the border zero
+
         r2_old = r2_new
+
     return u, v, it, r2_old / r2_0
