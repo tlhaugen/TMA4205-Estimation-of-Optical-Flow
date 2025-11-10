2e4f149029fe73b11d6a54d0bbe0eebf32465c05
Tor <157131052+tlhaugen@users.noreply.github.com>
2025-11-10 13:22:39 +0100

    Rewrote cg to use interior, implied dirichlet

--- utilities.py
+++ utilities.py
@@ -1,111 +1,88 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter
 
+def interior_image(I):
+    return I[1:-1, 1:-1]
 
-def forward_diff_x_boundary(I):
+def smooth(I, sigma):    
+    return gaussian_filter(I, sigma)
+
+def forward_diff_boundary(I):
     n, m = I.shape
     dx = np.zeros_like(I)
+    dy = np.zeros_like(I)
+
     dx[:, :m-1] = I[:, 1:] - I[:, :-1]
     dx[:, m-1] = I[:, m-1] - I[:, m-2]
-    return dx
 
-def forward_diff_y_boundary(I):
-    n, m = I.shape
-    dy = np.zeros_like(I)
     dy[:n-1, :] = I[1:, :] - I[:-1, :]
     dy[n-1, :] = I[n-1, :] - I[n-2, :]
-    return dy
+    return dx, dy
 
 def compute_derivatives(I0, I1):
-    Ix = 0.5 * (forward_diff_x_boundary(I0) + forward_diff_x_boundary(I1))
-    Iy = 0.5 * (forward_diff_y_boundary(I0) + forward_diff_y_boundary(I1))
+    Ix0, Iy0 = forward_diff_boundary(I0)
+    Ix1, Iy1 = forward_diff_boundary(I1)
+
+    Ix = 0.5 * (Ix0 + Ix1)
+    Iy = 0.5 * (Iy0 + Iy1)
+
     It = I1 - I0
     return Ix, Iy, It
 
-def maybe_smooth(I, sigma):
-    if sigma and sigma > 0.0:
-        from scipy.ndimage import gaussian_filter
-        return gaussian_filter(I, sigma=sigma)
-    return I
-
-def compute_derivatives_blur(I0, I1, sigma):
-    I0b = maybe_smooth(I0, sigma)
-    I1b = maybe_smooth(I1, sigma)
-    return compute_derivatives(I0b, I1b)
 
 def build_rhs(Ix, Iy, It):
     return -It * Ix, -It * Iy
 
 
-def laplacian5_dirichlet(u):
+def laplacian5(u):
     n, m = u.shape
     L = np.zeros_like(u)
     L[1:n-1, 1:m-1] = (
         u[0:n-2, 1:m-1] + u[2:n, 1:m-1] +
-        u[1:n-1, 0:m-2] + u[1:n-1, 2:m] -
-        4.0 * u[1:n-1, 1:m-1]
-    )
-    L[0, :] = 0.0; L[-1, :] = 0.0; L[:, 0] = 0.0; L[:, -1] = 0.0
+        u[1:n-1, 0:m-2] + u[1:n-1, 2:m] - 4.0 * u[1:n-1, 1:m-1])
     return L
 
 def apply_A(u, v, Ix, Iy, reg):
-    Lu = laplacian5_dirichlet(u)
-    Lv = laplacian5_dirichlet(v)
+    Lu = laplacian5(u)
+    Lv = laplacian5(v)
     Au = (Ix * Ix) * u + (Ix * Iy) * v - reg * Lu
     Av = (Ix * Iy) * u + (Iy * Iy) * v - reg * Lv
- 
-    Au[0, :] = 0.0; Au[-1, :] = 0.0; Au[:, 0] = 0.0; Au[:, -1] = 0.0
-    Av[0, :] = 0.0; Av[-1, :] = 0.0; Av[:, 0] = 0.0; Av[:, -1] = 0.0
     return Au, Av
 
-def enforce_bc(u, v):
-    u[0, :] = 0.0; u[-1, :] = 0.0; u[:, 0] = 0.0; u[:, -1] = 0.0
-    v[0, :] = 0.0; v[-1, :] = 0.0; v[:, 0] = 0.0; v[:, -1] = 0.0
 
-
-def of_cg(u0, v0, Ix, Iy, reg, rhsu, rhsv, tol=1e-8, maxit=2000, callback=None):
-    u = u0.copy(); v = v0.copy()
-
-    # initial residual
+def of_cg(u0, v0, Ix, Iy, reg, rhsu, rhsv, tol=1e-8, maxit=2000):
+    u = u0.copy()
+    v = v0.copy()
     Au, Av = apply_A(u, v, Ix, Iy, reg)
+
     ru = rhsu - Au
     rv = rhsv - Av
-    r2_0 = float(np.sum(ru * ru + rv * rv))
-    if r2_0 == 0.0:
-        enforce_bc(u, v)
-        return u, v, 0, 0.0
+    r2_0 = np.vdot(ru, ru) + np.vdot(rv, rv)
 
-    pu = ru.copy(); pv = rv.copy()
-    enforce_bc(pu, pv)
+    pu = ru.copy()
+    pv = rv.copy()
+    r2_old = r2_0.copy()
 
-    r2_old = r2_0
-    for it in range(1, maxit + 1):
-        Apu, Apv = apply_A(pu, pv, Ix, Iy, reg)
-        denom = float(np.sum(pu * Apu + pv * Apv))
-        if denom <= 0.0:
-            break
+    for it in range(maxit):
+        Ap_u, Ap_v = apply_A(pu, pv, Ix, Iy, reg)
 
-        alpha = r2_old / denom
+        alpha = r2_old / (np.vdot(pu, Ap_u) + np.vdot(pv, Ap_v))       # (r_k^T r_K) / (p_k^T A p_k)
 
-        # update and enforce Dirichlet on the solution
-        u += alpha * pu
+        u += alpha * pu # update solution
         v += alpha * pv
-        enforce_bc(u, v)
 
-        # residual
-        ru -= alpha * Apu
-        rv -= alpha * Apv
+        ru -= alpha * Ap_u  # residual
+        rv -= alpha * Ap_v
 
-        r2_new = float(np.sum(ru * ru + rv * rv))
+        r2_new = np.vdot(ru, ru) + np.vdot(rv, rv)
         rel = r2_new / r2_0
-        if callback is not None:
-            callback(it, rel)
+
         if rel < tol:
             return u, v, it, rel
 
         beta = r2_new / r2_old
         pu = ru + beta * pu
         pv = rv + beta * pv
-        enforce_bc(pu, pv)  # keep the border zero
 
         r2_old = r2_new
 
